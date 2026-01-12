80561805607597d56fdd9a4f0308a990b8ca3554
Kevin Abatan <abatan.k@gmail.com>
2026-01-12 02:07:28 +0100

    fix: examples

--- src/registry/examples/pagination-example.tsx
+++ src/registry/examples/pagination-example.tsx
@@ -8,6 +8,8 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/registry/ui/pagination";
+import { Field, FieldLabel } from "../ui/field";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 export default function PaginationExample() {
   return (
@@ -79,18 +81,46 @@ function PaginationSimple() {
 }
 
 function PaginationIconsOnly() {
+  const items = [
+    { value: "10", label: "10" },
+    { value: "25", label: "25" },
+    { value: "50", label: "50" },
+    { value: "100", label: "100" },
+  ];
+
   return (
-    <Example title="Icons Only">
-      <Pagination class="mx-0 w-auto">
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+    <Example title="With Select">
+      <div class="flex items-center justify-between gap-4">
+        <Field orientation="horizontal" class="w-fit">
+          <FieldLabel for="select-rows-per-page">Rows per page</FieldLabel>
+          <Select<(typeof items)[number]>
+            options={items}
+            optionValue="value"
+            optionTextValue="label"
+            defaultValue={items[1]}
+            itemComponent={(props) => (
+              <SelectItem item={props.item}>{props.item.rawValue.label}</SelectItem>
+            )}
+          >
+            <SelectTrigger class="w-20" id="select-rows-per-page">
+              <SelectValue<(typeof items)[number]>>
+                {(state) => state.selectedOption().label}
+              </SelectValue>
+            </SelectTrigger>
+            <SelectContent />
+          </Select>
+        </Field>
+        <Pagination class="mx-0 w-auto">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </Example>
   );
 }
