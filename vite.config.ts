48755c8764b65e33d59fe32f205ef1a6968ca52a
Kevin Abatan <abatan.k@gmail.com>
2026-01-15 04:01:16 +0100

    fix: use bun to access env variables

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import tailwind from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
 import velite from "@velite/plugin-vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import paths from "vite-tsconfig-paths";
 import mdx from "./src/lib/vite-plugins/mdx";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
-    port: parseInt(loadEnv(mode, "../", "").FRONTEND_PORT || "5175", 10),
+    port: parseInt(Bun.env.FRONTEND_PORT || "5175", 10),
   },
   plugins: [
     mdx({
@@ -30,4 +30,4 @@ export default defineConfig(({ mode }) => ({
     solid({ ssr: true, hot: true, extensions: [".tsx", ".jsx", ".mdx"] }),
     velite(),
   ],
-}));
+});
