08f41d448b45d1539cf36fb3072eed403cf0c34d
Kevin Abatan <abatan.k@gmail.com>
2026-01-20 00:54:29 +0100

    fix: bad styling for arrow

--- src/registry/ui/tooltip.tsx
+++ src/registry/ui/tooltip.tsx
@@ -9,7 +9,6 @@ const Tooltip = (props: TooltipPrimitive.TooltipRootProps) => {
     {
       openDelay: 0,
       placement: "top",
-      gutter: 4,
     } as TooltipPrimitive.TooltipRootProps,
     props,
   );
@@ -38,13 +37,13 @@ const TooltipContent = <T extends ValidComponent = "div">(props: TooltipContentP
       <TooltipPrimitive.Content
         data-slot="tooltip-content"
         class={cn(
-          "cn-tooltip-content z-50 w-fit max-w-xs origin-(--kb-popover-content-transform-origin) bg-foreground text-background",
+          "cn-tooltip-content z-50 w-fit max-w-xs origin-(--kb-tooltip-content-transform-origin) bg-foreground text-background",
           local.class,
         )}
         {...others}
       >
         {local.children}
-        <TooltipPrimitive.Arrow class="cn-tooltip-arrow z-50 bg-foreground fill-foreground data-[side=bottom]:top-1 data-[side=left]:top-1/2! data-[side=right]:top-1/2! data-[side=left]:-right-1 data-[side=top]:-bottom-2.5 data-[side=right]:-left-1 data-[side=left]:-translate-y-1/2 data-[side=right]:-translate-y-1/2" />
+        <TooltipPrimitive.Arrow size={19} />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   );
