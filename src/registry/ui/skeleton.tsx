1e4a71d3395fb743d07a44c594c10bcde28ba32a
Kevin Abatan <abatan.k@gmail.com>
2026-01-19 13:34:44 +0100

    refactor: remove kobalte primitive for skeleton

--- src/registry/ui/skeleton.tsx
+++ src/registry/ui/skeleton.tsx
@@ -1,24 +1,12 @@
-import type { PolymorphicProps } from "@kobalte/core/polymorphic";
-import * as SkeletonPrimitive from "@kobalte/core/skeleton";
-import type { ComponentProps, ValidComponent } from "solid-js";
+import type { ComponentProps } from "solid-js";
 import { splitProps } from "solid-js";
 
 import { cn } from "@/lib/utils";
 
-type SkeletonProps<T extends ValidComponent = "div"> = PolymorphicProps<
-  T,
-  SkeletonPrimitive.SkeletonRootProps<T>
-> &
-  Pick<ComponentProps<T>, "class">;
-
-const Skeleton = <T extends ValidComponent = "div">(props: SkeletonProps<T>) => {
-  const [local, others] = splitProps(props as SkeletonProps, ["class"]);
+const Skeleton = (props: ComponentProps<"div">) => {
+  const [local, others] = splitProps(props, ["class"]);
   return (
-    <SkeletonPrimitive.Root
-      class={cn("cn-skeleton animate-pulse", local.class)}
-      data-slot="skeleton"
-      {...others}
-    />
+    <div class={cn("cn-skeleton animate-pulse", local.class)} data-slot="skeleton" {...others} />
   );
 };
 
