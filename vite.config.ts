68c77dbcfabdbc828daf794ed9996d0a67557944
Kevin Abatan <abatan.k@gmail.com>
2026-01-26 17:18:26 +0100

    chore: specify port in the server entry in vite config js

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,36 @@ import tailwind from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
 import velite from "@velite/plugin-vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import solid from "vite-plugin-solid";
 import paths from "vite-tsconfig-paths";
 import mdx from "./src/lib/vite-plugins/mdx";
 
-export default defineConfig({
-  plugins: [
-    mdx({
-      jsx: true,
-      jsxImportSource: "solid-js",
-      providerImportSource: "solid-mdx",
-      stylePropertyNameCase: "css",
-    }),
-    devtools(),
-    paths({ projects: ["./tsconfig.json"] }),
-    tailwind(),
-    tanstackStart({
-      prerender: {
-        enabled: true,
-        crawlLinks: true,
-      },
-    }),
-    solid({ ssr: true, hot: true, extensions: [".tsx", ".jsx", ".mdx"] }),
-    velite(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      port: Number(env.FRONTEND_PORT ?? 5175),
+    },
+    plugins: [
+      mdx({
+        jsx: true,
+        jsxImportSource: "solid-js",
+        providerImportSource: "solid-mdx",
+        stylePropertyNameCase: "css",
+      }),
+      devtools(),
+      paths({ projects: ["./tsconfig.json"] }),
+      tailwind(),
+      tanstackStart({
+        prerender: {
+          enabled: true,
+          crawlLinks: true,
+        },
+      }),
+      solid({ ssr: true, hot: true, extensions: [".tsx", ".jsx", ".mdx"] }),
+      velite(),
+    ],
+  };
 });
