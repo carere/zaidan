2f0beb3e97655fd1434d2c0994d4a3ed35cd3a92
Kevin Abatan <1385328+carere@users.noreply.github.com>
2026-02-03 00:15:28 +0100

    fix: some ssr quircks to tackle

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
+import { cookieStorage } from "@solid-primitives/storage";
+import { createIsomorphicFn } from "@tanstack/solid-start";
+import { getRequestHeaders, getResponseHeaders } from "@tanstack/solid-start/server";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getStorage = createIsomorphicFn()
+  .server(() =>
+    cookieStorage.withOptions({
+      expires: new Date(Date.now() + 3e10),
+      getRequestHeaders: () => getRequestHeaders(),
+      getResponseHeaders: () => getResponseHeaders(),
+    }),
+  )
+  .client(() =>
+    cookieStorage.withOptions({
+      expires: new Date(Date.now() + 3e10),
+    }),
+  );
