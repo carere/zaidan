e0c1512264f65e8efc1a28872af6a0496bf630f5
Kevin Abatan <abatan.k@gmail.com>
2026-05-20 00:00:27 +0200

    chore: update deps and qa

--- vite.config.ts
+++ vite.config.ts
@@ -3,35 +3,28 @@ import tailwind from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
 import velite from "@velite/plugin-vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import lucide from "vite-plugin-lucide-preprocess";
 import solid from "vite-plugin-solid";
 import mdx from "./src/lib/vite-plugins/mdx";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    resolve: {
-      tsconfigPaths: true,
-    },
-    server: {
-      port: env.CONDUCTOR_PORT ? Number(env.CONDUCTOR_PORT) : undefined,
-    },
-    plugins: [
-      lucide(),
-      mdx({
-        jsx: true,
-        jsxImportSource: "solid-js",
-        providerImportSource: "solid-mdx",
-        stylePropertyNameCase: "css",
-      }),
-      devtools(),
-      cloudflare({ viteEnvironment: { name: "ssr" } }),
-      tailwind(),
-      tanstackStart(),
-      solid({ ssr: true, hot: true, extensions: [".tsx", ".mdx"] }),
-      velite(),
-    ],
-  };
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    lucide(),
+    mdx({
+      jsx: true,
+      jsxImportSource: "solid-js",
+      providerImportSource: "solid-mdx",
+      stylePropertyNameCase: "css",
+    }),
+    devtools(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tailwind(),
+    tanstackStart(),
+    solid({ ssr: true, hot: true, extensions: [".tsx", ".mdx"] }),
+    velite(),
+  ],
 });
