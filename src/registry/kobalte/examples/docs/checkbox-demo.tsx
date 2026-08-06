87033f526e34a2b7cca6c8e1abd3c19bbb16de3e
Kevin Abatan <abatan.k@gmail.com>
2026-08-06 08:34:09 +0000

    docs(checkbox): port focused demos

---
+++ src/registry/kobalte/examples/docs/checkbox-demo.tsx
@@ -0,0 +1,42 @@
+import { Checkbox } from "@/registry/kobalte/ui/checkbox";
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldTitle,
+} from "@/registry/kobalte/ui/field";
+
+export default function CheckboxDemo() {
+  return (
+    <FieldGroup class="max-w-sm">
+      <Field orientation="horizontal">
+        <Checkbox id="terms-checkbox" name="terms-checkbox" />
+        <FieldLabel for="terms-checkbox">Accept terms and conditions</FieldLabel>
+      </Field>
+      <Field orientation="horizontal">
+        <Checkbox id="terms-checkbox-2" name="terms-checkbox-2" defaultChecked />
+        <FieldContent>
+          <FieldLabel for="terms-checkbox-2">Accept terms and conditions</FieldLabel>
+          <FieldDescription>By clicking this checkbox, you agree to the terms.</FieldDescription>
+        </FieldContent>
+      </Field>
+      <Field orientation="horizontal" data-disabled>
+        <Checkbox id="toggle-checkbox" name="toggle-checkbox" disabled />
+        <FieldLabel for="toggle-checkbox">Enable notifications</FieldLabel>
+      </Field>
+      <FieldLabel>
+        <Field orientation="horizontal">
+          <Checkbox id="toggle-checkbox-2" name="toggle-checkbox-2" />
+          <FieldContent>
+            <FieldTitle>Enable notifications</FieldTitle>
+            <FieldDescription>
+              You can enable or disable notifications at any time.
+            </FieldDescription>
+          </FieldContent>
+        </Field>
+      </FieldLabel>
+    </FieldGroup>
+  );
+}
