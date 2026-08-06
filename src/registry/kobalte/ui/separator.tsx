427b00e8d7eb59813a013a63aade2d4e4d4d0d59
Kevin Abatan <abatan.k@gmail.com>
2026-08-06 03:45:44 +0000

    fix(separator): synchronize component (#340)

--- src/registry/kobalte/ui/separator.tsx
+++ src/registry/kobalte/ui/separator.tsx
@@ -3,17 +3,24 @@ import { Separator as SeparatorPrimitive, type SeparatorRootProps } from "@kobal
 import { type ComponentProps, mergeProps, splitProps, type ValidComponent } from "solid-js";
 import { cn } from "@/lib/utils";
 
-type SeparatorProps<T extends ValidComponent = "hr"> = PolymorphicProps<T, SeparatorRootProps<T>> &
+type SeparatorProps<T extends ValidComponent = "div"> = PolymorphicProps<T, SeparatorRootProps<T>> &
   Pick<ComponentProps<T>, "class">;
 
-const Separator = <T extends ValidComponent = "hr">(props: SeparatorProps<T>) => {
-  const mergedProps = mergeProps({ orientation: "horizontal" } as const, props);
-  const [local, others] = splitProps(mergedProps as SeparatorProps, ["class"]);
+const Separator = <T extends ValidComponent = "div">(props: SeparatorProps<T>) => {
+  const mergedProps = mergeProps({ as: "div", orientation: "horizontal" } as const, props);
+  const [local, others] = splitProps(mergedProps as SeparatorProps, ["class", "orientation"]);
+
   return (
     <SeparatorPrimitive
       data-slot="separator"
+      role="separator"
+      aria-orientation={local.orientation}
+      orientation={local.orientation}
       class={cn(
-        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
+        "z-separator shrink-0 bg-border",
+        local.orientation === "horizontal"
+          ? "z-separator-horizontal h-px w-full"
+          : "z-separator-vertical w-px self-stretch",
         local.class,
       )}
       {...others}
